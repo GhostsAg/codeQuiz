c620e543a6aab1d17c9583fb985a90a8bdde0758
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-02-09 18:40:56 -0600

    Added all functioning logic; need timer fix

--- assets/logic.js
+++ assets/logic.js
@@ -10,7 +10,10 @@ var first = new Quiz("How do we set a new unchanging variable in javascript?", "
 var second = new Quiz("How do we access the target property of an event listener?", "event.target", ["listener.target", "property.target", "property(event)"]);
 var third = new Quiz("Who invented javascript?", "Brendan Eich", ["Mark Zuckerberg", "Guido van Rossum", "Elon Musk"]);
 var fourth = new Quiz("How long did it take Brendan Eich to invent javascript?", "10 days", ["10 weeks", "10 months", "10 years"]);
-var five = new Quiz("How is javascript code executed inside the web browser?", "using a JIT compiler called V8", ["using your IDE debugger", "using the g++ compiler", "using the web browser console"]);
+var fifth = new Quiz("How is javascript code executed inside the web browser?", "using a JIT compiler called V8", ["using your IDE debugger", "using the g++ compiler", "using the web browser console"]);
+
+//  Array of all content with questions and answers
+var Quiz = [first, second, third, fourth, fifth];
 
 // Element variables
 
@@ -36,6 +39,102 @@ var $initials = document.getElementById("initials");
 // View high scores page
 var $highScores = document.getElementById("highScores");
 var $scores = document.getElementById("scores");
+
+//  Global variables
+let timerVar = 60;
+var initialsVar;
+var startTimer;
+var iNum = 0;
+
+function funcTimer(timer) {
+    switch(timer) {
+        case timer < 1:
+            clearInterval(startTimer);
+            $content.style.display = "none";
+            $setHighScores.style.display = "block";
+        default:
+            timer--;
+            $timer.textContent = `${timer}`; 
+    }
+}
+
+function begin(event) {
+    event.preventDefault();
+    $landing.style.display = "none";
+    $content.style.display = "block";
+    startTimer = setInterval(funcTimer, 1000, timerVar);
+    $question.textContent = Quiz[0].question;
+    $answers[0].textContent = Quiz[0].answerRight;
+    for (let i=1; i<$answers.length; i++) {
+        $answers[i].textContent = Quiz[0].answerWrong[i-1]; 
+    }
+}
+
+function viewHigh() {
+    $landing.style.display = "none";
+    $content.style.display = "none";
+    $setHighScores.style.display = "none";
+    $highScores.style.display = "block";
+}
+
+function hideFeedback () {
+    $feedBack.style.display = "none";
+}
+
+function check(event) {
+    if (Quiz[iNum].answerWrong.includes(event.target.innerHTML)){
+        $feedBack.style.display = "block";
+        $feedBack.innerHTML = "Wrong!";
+        setTimeout(hideFeedback, 500);
+        timerVar -= 5;
+        $timer.textContent = `${timerVar}`;
+    }
+    else if (event.target.innerHTML.includes(Quiz[4].answerRight)) {
+        clearInterval(startTimer);
+        $content.style.display = "none";
+        $setHighScores.style.display = "block";
+    }
+    else {
+        iNum++;
+        $feedBack.style.display = "block";
+        $feedBack.innerHTML = "Right!";
+        setTimeout(hideFeedback, 500);
+        $question.textContent = Quiz[iNum].question;
+        $answers[0].textContent = Quiz[iNum].answerRight;
+        for (let i=1; i<$answers.length; i++) {
+            $answers[i].textContent = Quiz[iNum].answerWrong[i-1]; 
+        }
+    }
+}
+
+function initials() {
+    initialsVar = $initials.value;
+    scoreVar = `${initialsVar} - ${timerVar}`;
+    $setHighScores.style.display = "none";
+    $highScores.style.display = "block";
+    var $newScore = document.createElement("li");
+    $newScore.textContent = scoreVar;
+    $scores.appendChild($newScore);
+    localStorage.setItem("score", scoreVar);
+}
+
+$beginBtn.addEventListener("click", begin, event);
+$viewHighBtn.addEventListener("click", viewHigh);
+
+//  Quiz time!
+for (let i=0; i<$answers.length; i++) {
+    $answers[i].addEventListener("click", check, event);
+}
+
+$submitBtn.addEventListener("click", initials);
+
+
+
+
+
+
+
+
 
 
 
